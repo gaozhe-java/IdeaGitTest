0bbf85013e28ca8929a942eac55522138dce006d
gaozhe-java <78540288+gaozhe-java@users.noreply.github.com>
2021-02-05 11:09:28 +0800

    Update Hello.java

--- src/main/java/cn/gz/Hello.java
+++ src/main/java/cn/gz/Hello.java
@@ -2,6 +2,6 @@ package cn.gz;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello git!!");
+        System.out.println("hello git!! edit by gzzz");
     }
 }
